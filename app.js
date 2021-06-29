ccc66f5d2efa86bdb9210267139b2b31079acb57
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-29 15:08:33 -0700

    added working user consent to quiz and question prompts

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ const quizButton = document.getElementById('quiz-button');
 
 // set event listeners 
 quizButton.addEventListener('click', ()=>{
-  console.log('You clicked the button!');
-  // get user input
+// get user input
+    const confirmed = confirm('Would you like to start the quiz?');
+
+    if (confirmed){
+        const name = prompt('What is your name?');
+        const ans1 = prompt('Do I like disc golf?');
+        const ans2 = prompt('Do I like cooking?');
+        const ans3 = prompt('Do I like lounging with my cats?');
+        const ans4 = prompt('Do I like fried rice?');
+        console.log(name, ans1, ans2, ans3, ans4);
+        // let score = 0;
+  
+        // console.log('final score', score);
+    } else {
+        console.log('user cancelled');
+    }
+
   // use user input to update state 
   // update DOM to reflect the new state
 
-  // const confirmed = confirm('Would you like to start the quiz?');
-    // console.log(confirmed);
-    // if (confirmed){
-    //     const name = prompt('What is your name?');
-    //     const ans1 = prompt('Do I like disc golf?');
-    //     const ans2 = prompt('Do I like cooking?');
-    //     const ans3 = prompt('Do I like lounging with my cats?');
-    //     const ans4 = prompt('Do I like fried rice?');
-    //     console.log(name, ans1, ans2, ans3, ans4);
-    //     let score = 0;
-
-    //     console.log('final score', score);
-    // } else {
-    //     console.log('user cancelled');
-    // }
 });

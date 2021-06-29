0127e89ddeaf4a1de1b0d6043862d82e2332bea1
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-29 15:30:49 -0700

    quiz runs and state updated from user inputs

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import { isYes } from './utils.js';
 const quizButton = document.getElementById('quiz-button');
 // const results = document.getElementById('results');
 
-// initialize state
 
 // set event listeners 
 quizButton.addEventListener('click', ()=>{
-// get user input
+  // get user input
     const confirmed = confirm('Would you like to start the quiz?');
-
+  
+  // initialize state
     if (confirmed){
         const name = prompt('What is your name?');
         const ans1 = prompt('Do I like disc golf?');
@@ -17,14 +17,25 @@ quizButton.addEventListener('click', ()=>{
         const ans3 = prompt('Do I like lounging with my cats?');
         const ans4 = prompt('Do I like fried rice?');
         console.log(name, ans1, ans2, ans3, ans4);
-        // let score = 0;
-  
-        // console.log('final score', score);
+        let score = 0;
+        // use user input to update state 
+        if (isYes(ans1)) {
+            score++;
+        }
+        if (isYes(ans2)) {
+            score++;
+        }
+        if (isYes(ans3)) {
+            score++;
+        }
+        if (isYes(ans4)) {
+            score++;
+        }
+        console.log('final score', score);
     } else {
         console.log('user cancelled');
     }
 
-  // use user input to update state 
   // update DOM to reflect the new state
 
 });
